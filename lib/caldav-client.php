e8de95fca07c3b5cdc2435713941facd5d73934a
Gene Hawkins <texxasrulez@yahoo.com>
2020-07-19 20:16:50 -0700

    Update caldav-client.php

--- lib/caldav-client.php
+++ lib/caldav-client.php
@@ -22,7 +22,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once (dirname(__FILE__).'/vendor/autoload.php');
+require_once (dirname(__FILE__).'/../../../vendor/autoload.php');
 
 
 class caldav_client extends Sabre\DAV\Client
