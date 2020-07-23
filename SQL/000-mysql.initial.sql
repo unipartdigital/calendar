a9d86800d94d3ebe45579c3d3e5b88b8c3444f6f
Gene Hawkins <texxasrulez@yahoo.com>
2020-07-22 22:57:25 -0700

    WIP

--- SQL/000-mysql.initial.sql
+++ SQL/000-mysql.initial.sql
@@ -1,34 +1,33 @@
-CREATE TABLE IF NOT EXISTS `calendar_oauth_states` (
-  provider VARCHAR(255) NOT NULL,
-  client_config_id VARCHAR(255) NOT NULL,
-  user_id VARCHAR(255) NOT NULL,
-  scope VARCHAR(255) NOT NULL,
+CREATE TABLE calendar_oauth_states (
+  provider VARCHAR(128) NOT NULL,
+  client_config_id VARCHAR(128) NOT NULL,
+  user_id VARCHAR(128) NOT NULL,
+  scope VARCHAR(128) NOT NULL,
   issue_time INTEGER NOT NULL,
-  state VARCHAR(255) NOT NULL,
-  UNIQUE (provider, client_config_id, user_id, scope),
-  PRIMARY KEY (state)
+  state VARCHAR(128) NOT NULL PRIMARY KEY,
+  CONSTRAINT calendar_oauth_states_unique_key UNIQUE (provider, client_config_id, user_id, scope)
 );
 
-CREATE TABLE IF NOT EXISTS `calendar_oauth_access_tokens` (
-  provider VARCHAR(255) NOT NULL,
-  client_config_id VARCHAR(255) NOT NULL,
-  user_id VARCHAR(255) NOT NULL,
-  scope VARCHAR(255) NOT NULL,
+CREATE TABLE calendar_oauth_access_tokens (
+  provider VARCHAR(128) NOT NULL,
+  client_config_id VARCHAR(128) NOT NULL,
+  user_id VARCHAR(128) NOT NULL,
+  scope VARCHAR(128) NOT NULL,
   issue_time INTEGER NOT NULL,
-  access_token VARCHAR(255) NOT NULL,
-  token_type VARCHAR(255) NOT NULL,
+  access_token VARCHAR(128) NOT NULL,
+  token_type VARCHAR(128) NOT NULL,
   expires_in INTEGER DEFAULT NULL,
-  UNIQUE (provider, client_config_id, user_id, scope)
+  CONSTRAINT calendar_oauth_access_tokens_unique_key UNIQUE (provider, client_config_id, user_id, scope)
 );
 
-CREATE TABLE IF NOT EXISTS `calendar_oauth_refresh_tokens` (
-  provider VARCHAR(255) NOT NULL,
-  client_config_id VARCHAR(255) NOT NULL,
-  user_id VARCHAR(255) NOT NULL,
-  scope VARCHAR(255) NOT NULL,
+CREATE TABLE calendar_oauth_refresh_tokens (
+  provider VARCHAR(128) NOT NULL,
+  client_config_id VARCHAR(128) NOT NULL,
+  user_id VARCHAR(128) NOT NULL,
+  scope VARCHAR(128) NOT NULL,
   issue_time INTEGER NOT NULL,
-  refresh_token VARCHAR(255) DEFAULT NULL,
-  UNIQUE (provider, client_config_id, user_id, scope)
+  refresh_token VARCHAR(128) DEFAULT NULL,
+  CONSTRAINT calendar_oauth_refresh_tokens_unique_key UNIQUE (provider, client_config_id, user_id, scope)
 );
 
-REPLACE INTO `system` (`name`, `value`) VALUES ('calendar-version', '2016011300');
+REPLACE INTO `system` (`name`, `value`) VALUES ('texxasrulez-oauth2-version', '2020072000');
