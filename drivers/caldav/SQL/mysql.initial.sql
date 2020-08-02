4aaa48d84306551c16b67900ca2449f25fdd9148
Gene Hawkins <texxasrulez@yahoo.com>
2020-08-02 10:16:16 -0700

    SQL Tweaks

--- drivers/caldav/SQL/mysql.initial.sql
+++ drivers/caldav/SQL/mysql.initial.sql
@@ -1,22 +1,3 @@
-/*
- * CalDAV Client
- *
- * @author Gene Hawkins <texxasrulez@yahoo.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 CREATE TABLE IF NOT EXISTS `caldav_attachments` (
   `attachment_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `event_id` int(11) UNSIGNED NOT NULL DEFAULT '0',
